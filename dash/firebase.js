008613e4b7902e2fdec7af1751e70b040d4dc6d6
construtora-metrocasa <metrocasa.construtora@gmail.com>
2025-12-05 18:52:41 -0300

    Create firebase.js

---
+++ dash/firebase.js
@@ -0,0 +1,13 @@
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyCd8rhcFbHwNuqVqEa1GXQz21xOPN0zUMY",
+  authDomain: "dash-dbc8f.firebaseapp.com",
+  projectId: "dash-dbc8f",
+  storageBucket: "dash-dbc8f.firebasestorage.app",
+  messagingSenderId: "824232359810",
+  appId: "1:824232359810:web:f4c180e23592205625819b",
+  measurementId: "G-40Z4XQ6JYW"
+};
+
+firebase.initializeApp(firebaseConfig);
+</script>
